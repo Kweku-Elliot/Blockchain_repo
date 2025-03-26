d8ae4dd0a5a83451ec44fd3b9f52b8119d15a4da
Kweku-Elliot <elliotakpalu@gmail.com>
2025-03-26 02:00:11 +0000

    Added Terin  rust  contract

---
+++ solana/Terin.rs
@@ -0,0 +1,25 @@
+//rust solana program
+
+use solana_program::{
+    account_info::{next_account_info, AccountInfo},
+    entrypoint,
+    entrypoint::ProgramResult,
+    msg,
+    pubkey::Pubkey,
+};
+
+entrypoint!(process_instruction);
+
+pub fn process_instruction(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    _instruction_data: &[u8],
+) -> ProgramResult {
+    let accounts_iter = &mut accounts.iter();
+    let account = next_account_info(accounts_iter)?;
+
+    // Print a message to the Solana log
+    msg!("Hello, Solana! This is a basic contract");
+
+    Ok(())
+}
